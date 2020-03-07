672ca5cf1378e7572bd67d3f00caf956baa5cd12
Ian <yankirsh@yandex.ru>
2020-03-07 13:32:49 +0300

    New tasks tofo

--- mainpage/views.py
+++ mainpage/views.py
@@ -7,6 +7,13 @@ from django import views
 
 class MainPageView(views.View):
     def get(self, request):
+
+        if request.user.is_authenticated:
+
+            pass
+        else:
+            pass
+
         # data = request.body.decode()
         return render(
             request=request,
@@ -14,6 +21,45 @@ class MainPageView(views.View):
             # using=str(data)
         )
         # return {"item": data}
+    
+# TODO Ян делает эту часть проекта
+def task_board_page(request):
+    pass
+
+# TODO Миша делает эту часть проекта
+def main_page(request):
+
+    if request.user.is_authenticated:
+        pass # кинуть на борду редиректом
+
+    if request.method == 'POST':        
+        form_type = request.POST.get("form_type")
+
+        if form_type == "login_form":
+            user_login = request.POST.get("login")
+
+            # TODO: достать данные формы
+
+            # TODO : залогинить пользователя в его аккаунт если все ок или перекинуть его назад на главную если нет
+            
+            # TODO: После логина отправить чувака на борду
+
+        elif form_type == "registration_form":
+            # СОздать пользователя в базе (гуглим django create user account)
+            pass
+        else:
+
+            pass
+            # TODO редиректим чувака на главную (ошибка) not supported form exception
+
+    elif request.method == 'GET':
+        return render(
+            request=request,
+            template_name='mainpage/mainpage_template.html'
+            # using=str(data)
+        )
+    else:
+        # TODO: Not supported exception (4**?)
 
 
 def post_list(request):
